d89e5d34b90c49973ce799ad533fb9b5e48521de
Gabriel Braga Zamproni Lima <gbragazlima001@gmail.com>
2025-07-19 20:17:48 -0300

    verifica sucesso do payments-processor antes de registrar pagamento no redis

--- src/main/java/br/com/johnnysoft/johnny_rinha/services/PaymentsService.java
+++ src/main/java/br/com/johnnysoft/johnny_rinha/services/PaymentsService.java
@@ -1,5 +1,6 @@
 package br.com.johnnysoft.johnny_rinha.services;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import br.com.johnnysoft.johnny_rinha.models.Payment;
@@ -16,9 +17,13 @@ public class PaymentsService {
         this.redisService = redisService;
     }
 
-    public String sendPayment(Payment payment) {
-        String url = "http://payment-processor-fallback:8080/payments";
+public String sendPayment(Payment payment) {
+    String url = "http://payment-processor-fallback:8080/payments";
+    HttpStatusCode statusCode = restTemplate.postForEntity(url, payment, String.class).getStatusCode();
+    if (statusCode.is2xxSuccessful()) {
         this.redisService.save(payment);
-        return restTemplate.postForObject(url, payment, String.class);
+        return "OK";
     }
+    return "Falha no processamento do pagamento. Status: " + statusCode;
+}
 }
